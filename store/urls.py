c4334575684f9104de4b116f418d2d875f777706
mkapanzhi <mak1917132@yandex.ru>
2024-02-24 23:41:32 +0300

    fix url adress on get_product_from_store page

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', get_store_page, name='home'),
     path('add_product/', get_add_product_page, name='add_product'),
     path('desc/<int:id>/', get_description_page, name='desc'),
-    path('get_product_from_store/<int:id>', get_product_from_store, name='get_product_from_store')
+    path('<int:id>', get_product_from_store, name='get_product_from_store')
 ]
