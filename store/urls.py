bbe3ceaac710b457cf00021011ef19bddf56e4e7
mkapanzhi <mak1917132@yandex.ru>
2024-02-24 23:14:02 +0300

    add get product by store page

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import get_store_page, get_add_product_page, get_description_page
+from .views import get_store_page, get_add_product_page, get_description_page, get_product_from_store
 
 urlpatterns = [
     path('', get_store_page, name='home'),
     path('add_product/', get_add_product_page, name='add_product'),
-    path('desc/<int:id>/', get_description_page, name='desc')
+    path('desc/<int:id>/', get_description_page, name='desc'),
+    path('get_product_from_store/<int:id>', get_product_from_store, name='get_product_from_store')
 ]
